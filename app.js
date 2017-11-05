64096aed950af11bd0576572d1721b1b12e5b7a1
Emmanuel <wamuoe@gmail.com>
2017-11-05 16:43:58 +0100

    Setup App

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+
+app.get('/', function (req, res) {
+
+    res.send('OWWWWWWFARRRRRR NAAAAA!!!');
+});
+
+app.get('/api/:oruko', function (req, res) {
+    res.json({
+        kilode: req.params.oruko + ' Party Don start',
+        kilonso: req.params.oruko + ' Make we start to roll kopuuuuuu!!!!'
+    });
+});
+
+app.listen(port);
